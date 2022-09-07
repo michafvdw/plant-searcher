eec5609c66206af3e1e89196fd290ff15e495840
Micha <74900539+michafvdw@users.noreply.github.com>
2022-09-07 14:31:44 +0200

    search bar (does not work yet)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchComponent msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import SearchComponent from '../components/SearchComponent.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    SearchComponent
+}
 }
 </script>
